1c835c55d2c01a87bb387695d73b8310c4cdd37f
choiguen <daindj0@gmail.com>
2026-03-16 16:48:25 +0900

    Feat: 테스트 orm 모델 생성

---
+++ app/models/test.py
@@ -0,0 +1,12 @@
+# 테스트 ORM 모델. 실제 프로젝트에서는 삭제.
+
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base import Base
+
+class Test(Base):
+    __tablename__="test"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    message: Mapped[str] = mapped_column(String(255))

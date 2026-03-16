8aecabd5d3674cd0a9293e68ee33213f5d0abb0c
choiguen <daindj0@gmail.com>
2026-03-16 17:57:39 +0900

    Feat: 테스트용 엔드포인트 생성

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from app.db.session import engine
+from app.db.base import Base
+from app.models import Test
+from app.repos.test_repo import get_or_create_test
+from app.api.deps import get_db
 
 app = FastAPI()
 
+# @app.on_event("startup") # FastAPI의 이벤트 시스템을 사용하여 애플리케이션이 시작될 때 실행되는 함수를 정의할 수 있음.
+# def startup_event():
+#     # 여기에 데이터베이스 연결이나 초기화 작업 수행 가능.
+#     def on_startup():
+#         Base.metadata.create_all(bind=engine)  # 데이터베이스 테이블 생성
+
 @app.get("/")
+# 테스트 ORM 모델. 실제 프로젝트에서는 삭제.
 def test_route():
-    return {"message": "Successfully connected to the FastAPI application"}
+    return get_or_create_test(db=next(get_db()), message="Successfully connected to the database")

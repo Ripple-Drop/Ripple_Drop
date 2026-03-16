5234bf9bcff15d1e65a27a3e700ff07f3a1c660b
choiguen <daindj0@gmail.com>
2026-03-16 16:37:11 +0900

    Feat: db연결 세션 생성 및 Base_model class생성

---
+++ app/db/session.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+# SQLAlchemy 설정
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(DATABASE_URL, echo=True)  # echo=True는 SQL 쿼리를 로그로 출력.
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # 세션 클래스 생성
+

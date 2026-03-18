8bbd11d07022cb8e8233889175b0c79667deb76f
choi8154 <daindj0@gmail.com>
2026-03-18 11:37:00 +0900

    Refactor: lru_cache로 캐싱하여 메모리 최적화 및 Test코드 관련 수정 - 오류: Test 코드에서 env 기본값을 생성하기 전에 settings를 호출하여 ci에서 test가 터지는 오류가 있었음. - 해결: 함수 호출 형식으로 settings를 호출 시 함수만 정의되도록 수정. - 기타: lru_cache는 이미 만들어진 같은 인수의 객체를 재사용하여 메모리   최적화의 역할을 함. 다시 말해서 객체를 매번 호출할 때 다시 생성하지   않는다는 이야기.

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -21,5 +23,8 @@ class Settings(BaseSettings):
         case_sensitive=True,  # 환경 변수 이름 대소문자 구분 여부 설정
     )
 
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
 
-settings: Settings = Settings()  # type: ignore[call-arg]
+settings = get_settings()  # type: ignore[call-arg]

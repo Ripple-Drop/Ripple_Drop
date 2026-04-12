27785006bf470cd399f9904053f17bd74fc2b14b
dddops <Eopseum0@gmail.com>
2026-04-13 00:58:21 +0900

    Ci: 코드 포메팅함

--- app/models/User.py
+++ app/models/User.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import UTC, datetime
 
 from sqlalchemy import Column, DateTime, Integer, String
 
@@ -6,11 +6,11 @@ from app.db.base import Base
 
 
 class DBUser(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     email = Column(String, nullable=False)
-    total_score = Column(int, nullable=False, default=0)
-    current_level = Column(int, nullable=False, default=0)
-    created_at = Column(DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    total_score = Column(Integer, nullable=False, default=0)
+    current_level = Column(Integer, nullable=False, default=0)
+    created_at = Column(DateTime, nullable=False, default=lambda: datetime.now(UTC))

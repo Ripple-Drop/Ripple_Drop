fc27d61de66c6a5b6e17b726a401b6681f89ed7a
dddops <Eopseum0@gmail.com>
2026-04-13 02:49:59 +0900

    Ci 코드 포매팅

--- tests/models/test_user_model.py
+++ tests/models/test_user_model.py
@@ -1,32 +1,31 @@
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime, UTC
+from datetime import datetime
 
+import pytest
 from app.db.base import Base
 from app.models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 TEST_DB_URL = "sqlite:///./test.db"
 
 engine = create_engine(TEST_DB_URL)
 TestingSessionLocal = sessionmaker(bind=engine)
 
+
 @pytest.fixture(scope="function")
 def db_session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-    
+
     session = TestingSessionLocal()
     try:
         yield session
     finally:
         session.close()
 
+
 def test_db_user_creation(db_session):
-    user = User(
-        name="테스트",
-        email="test@mysql.com"
-    )
+    user = User(name="테스트", email="test@mysql.com")
     db_session.add(user)
     db_session.commit()
     queried_user = db_session.query(User).filter_by(name="테스트").first()

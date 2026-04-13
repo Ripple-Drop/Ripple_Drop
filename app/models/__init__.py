033a9cab322fb644017641c0459d9d334595b715
dddops <Eopseum0@gmail.com>
2026-04-13 22:58:31 +0900

    Feat: init 임포트

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,8 @@
 # 여기에 모델들을 다 import 하삼. main.py에서 들고오기 쉽게.
+
+from app.models.evaluation import Evaluation
+from app.models.messagelog import MessageLog
 from app.models.scenario import Scenario
 from app.models.user import User
 
-__all__ = ["Scenario", "User"]
+__all__ = ["Evaluation", "MessageLog", "Scenario", "User"]
